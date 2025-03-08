9296e783c87514f490f49ead581fbe6e1fe97aca
Claire-gip <chiara_boni@outlook.it>
2025-03-08 19:17:21 +0100

    adding imgs portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -50,41 +50,73 @@ function typingTerminal() {
 function contentModalPortfolio(projectName) {
     $('#portfolioModalLabel').text(projectName);
 
-    // on modal shown
-    $('#portfolioModal').on('shown.bs.modal', function (event) {
-
-    });
-
     let slide1 = document.getElementById("slide_one");
     let slide2 = document.getElementById("slide_two");
     let slide3 = document.getElementById("slide_three");
+    let slide4 = document.getElementById("slide_four");
     let ul = document.getElementById("tech_list");
     ul.innerHTML = '';
-    let names = [];
+    let text = document.getElementById('modal-text');
+    text.innerHTML = '';
+    let tools = [];
+    let description = '';
 
     switch(projectName) {
         case 'dalloways':
             slide1.src="./assets/img/portfolio/dalloways/frame1.png";
             slide2.src="./assets/img/portfolio/dalloways/frame2.png";
             slide3.src="./assets/img/portfolio/dalloways/frame3.png";
-            names = ['GDevelop', 'MidJourney'];
+            slide4.src="./assets/img/portfolio/dalloways/frame4.png";
+            tools = ['GDevelop - ', 'MidJourney'];
+            description = '<p>“DalloWays - Le vie di Mrs. Dalloway” is a <b>side-view</b> adventure game that follows the main character,' + 
+                ' Clarissa Dalloway, during a walk in London during June 13, 1923. </p>' +
+                'The play is inspired by Virginia Woolf\'s novel, <b>Mrs. Dalloway</b>, with the aim of bringing the audience closer to one' +
+                ' of the classics of contemporary literature.<br> '+
+                '<p>The player progresses by exploring different areas of <b>London</b>, interacting with various characters and collecting fragments of stories and reflections,'+
+                ' following the plot of the book.</p>' +
+                'There is a “catching objects” <b>mini-game</b> to simulate the purchase of the bouquet of flowers that Clarissa picks up for her party.';
             break;
 
-        case 'riot':
+        case 'cento':
+            slide1.src="./assets/img/portfolio/riot/frame1.png";
+            slide2.src="./assets/img/portfolio/riot/frame2.png";
+            slide3.src="./assets/img/portfolio/riot/frame3.png";
+            slide4.src="./assets/img/portfolio/riot/frame4.png";
+            tools = ['Adobe After Effects - ', 'Blender'];
             break;
 
-        case 'medusa':
+        case 'medusa': 
+            slide1.src="./assets/img/portfolio/medusa/frame1.png";
+            slide2.src="./assets/img/portfolio/medusa/frame2.png";
+            slide3.src="./assets/img/portfolio/medusa/frame3.png";
+            slide4.src="./assets/img/portfolio/medusa/frame4.png";
+            tools = ['MidJourney'];
+
             break;
 
         case 'palisade':
+            slide1.src="./assets/img/portfolio/palisade/frame1.png";
+            slide2.src="./assets/img/portfolio/palisade/frame2.png";
+            slide3.src="./assets/img/portfolio/palisade/frame3.png";
+            slide4.src="./assets/img/portfolio/palisade/frame4.png";
+            tools = ['C++ - ', 'Palisade - ', 'Python - ', 'Pandas'];
+
             break;
 
         case 'smartweets':
+            slide1.src="./assets/img/portfolio/smartweets/frame1.png";
+            slide2.src="./assets/img/portfolio/smartweets/frame2.png";
+            slide3.src="./assets/img/portfolio/smartweets/frame3.png";
+            slide4.src="./assets/img/portfolio/smartweets/frame4.png";
+            tools = ['Android Studio - ', 'Java - ', 'Python' ];
             break;
     }
 
-    for (var i = 0; i < names.length; i++) {
-        var name = names[i];
+    text.innerHTML = description;
+
+    // tools
+    for (var i = 0; i < tools.length; i++) {
+        var name = tools[i];
         var li = document.createElement('li');
         li.classList.add('list-inline-item');
         li.appendChild(document.createTextNode(name));
